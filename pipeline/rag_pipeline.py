69ad4f6ff27056efd1050248188803e0a1e26304
Amit Giri <amit101giri@gmail.com>
2025-01-30 13:15:43 +0545

    documented pipeline

--- pipeline/rag_pipeline.py
+++ pipeline/rag_pipeline.py
@@ -1,19 +1,31 @@
-import sys
-import os
-
-# Add the parent directory (RAGImplementation) to sys.path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from retriever.retriever import Retriever
 from generator.generator import Generator
 from generator.prompt_templates import create_prompt
 
 class RAGPipeline:
+    """Combines the retriever and generator to work together.
+    """
     def __init__(self, retriever_model=None, generator_model=None):
+        """Initialize the retriever and generator.
+
+        Args:
+            retriever_model (str, optional): Name of the retriever model. Defaults to None.
+            generator_model (str, optional): Name of the generator model. Defaults to None.
+        """
         self.retriever = Retriever(retriever_model)
         self.generator = Generator(generator_model)
 
-    def run(self, query, file_path, top_k=10):
+    def run(self, query:str, file_path:str, top_k=10) -> str:
+        """Returns the response based on query and content of given file using llm.
+
+        Args:
+            query (_type_): User's actual query.
+            file_path (_type_): Path of the file or document used to retrieve content related to the query.
+            top_k (int, optional): Number of top matched content of document with query to return. Defaults to 10.
+
+        Returns:
+            str: Response from the generator. 
+        """
         self.retriever.index_documents(file_path)
         documents = self.retriever.search(query, top_k)
         prompt = create_prompt(query, documents)
