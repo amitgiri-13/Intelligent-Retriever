17ab4f0f37278df84148cd0c9cb6bad1e897743a
Amit Giri <amit101giri@gmail.com>
2025-02-16 14:30:04 +0545

    fixes

--- generator/prompt_templates.py
+++ generator/prompt_templates.py
@@ -8,6 +8,4 @@ def create_prompt(query: str, context: str):
     Returns:
         _type_: Custom prompt for  llm.
     """
-    context = f"\nBased on the context answer query like wise human\n"
-    context = context.join(context)
     return f"Context:\n{context}\nQuery:\n{query}\nAnswer:"
